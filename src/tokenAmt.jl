a2db831290b5138a7c13d8acfe35b05801bbe643
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2025-05-04 23:18:52 -0300

    UPD the src/tokenAmt.jl

--- src/tokenAmt.jl
+++ src/tokenAmt.jl
@@ -2,4 +2,17 @@
 #                                           tokenAmt.jl                                            #
 #--------------------------------------------------------------------------------------------------#
 
+using FixedPointDecimals
+using Printf
+
+"""
+`struct amt <: PlainAmount`\n
+Plain, uniform precision, uniform underlying data type, fixed point decimal "amt" amount.
+"""
+struct amt <: PlainAmount
+    val::FixedDecimal{DEF.FixDecTy, DEF.decimals}
+end
+
+# export
+export amt
 
