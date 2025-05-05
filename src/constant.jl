233f8ed9d9a3be33ea2e6af41d1650dd4800dbba
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2025-05-04 23:25:22 -0300

    UPD the src/constant.jl

--- src/constant.jl
+++ src/constant.jl
@@ -9,6 +9,9 @@ Constant definitions for the package.
 const DEF = (decimals = 10,
              FixDecTy = Int128,)
 
+# Resulting denominator
+DENO = DEC.FixDecTy(10)^DEF.decimals
+
 # export
 export DEF
 
